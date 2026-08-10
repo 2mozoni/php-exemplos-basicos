b66c0d8a23ebe1d7a3feac09ba8e64d564336056
2mozoni <pemozoni8@gmail.com>
2026-08-10 10:24:40 -0300

    laços e vetores
    while, do while e arrays

--- inicio-revisao/4_lacos.php
+++ inicio-revisao/4_lacos.php
@@ -4,3 +4,21 @@
 for($i = 1; $i <= 10; $i++) {
     echo "8 x $i =" . (8 * $i) . "\n";
 }
+
+// While - (enquanto contagem regressiva)
+ echo "<br>";
+$n = 5;
+while ($n > 0) {
+    echo $n. "<br>";
+    $n--;
+}
+echo "<br>";
+
+// Do while - (faça enquanto) executa ao menos 1 vez
+$x = 10;
+do {
+    echo "x vale: $x "
+    $x++;
+} while ($x < 10);
+
+
---
+++ inicio-revisao/5_vetores.php
@@ -0,0 +1,29 @@
+<?php
+
+// vetor (array simples)
+$frutas = ["maçã","banana", "uva"];
+
+// exibindo (usando laço)
+foreach ($frutas as $indice => $frutas) {
+    echo "Posição $indice: $frutas\n";
+}
+
+//  Matriz (array completo "linhas e colunas)
+$matriz = [
+    ["Max Verstappen", "Lando Norris", "Oscar Piastri"],
+    ["Charles Leclerc", "Lewis Hamilton", "George Russel"],
+    ["Fernando Alonso", "Carlos Sainz", "Ayrton Senna"]
+];
+
+// exibindo nome dos pilotos
+echo "<br><br>";
+echo "melhores pilotos de f1: <br><br>";
+foreach ($matrizas $linha) {
+    foreach ($linha as $piloto) {
+        echo $piloto . " | ";
+        }
+        echo "<br>";
+}
+
+    
+
