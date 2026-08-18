f19b69933378109c72eabf86476522f6196d94db
2mozoni <pemozoni8@gmail.com>
2026-08-18 16:11:33 -0300

    update  assunto 2
    Cadastro

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Status</title>
+</head>
+<body>
+    
+<h1>Cadastro de Alunos (com status code)</h1>
+<form action="" method="post">
+
+<label> for="nome">Nome do aluno:</label>
+<input type="text" name="nome" id="nome" required><br><br>
+
+<label> for="idade">idade:</label>
+<input type="text" name="idade"  required><br><br>
+<button type="submit">enviar</button>
+
+</form>
+
+</body>
+</html>
